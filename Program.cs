d2e0f5b908c80c21ace4c8c55579df7386587cf6
Oscar Olsson <happybits@gmail.com>
2025-01-16 18:39:56 +0100

    -

--- Program.cs
+++ Program.cs
@@ -1,45 +1,46 @@
 using Microsoft.EntityFrameworkCore;
 using MyWebApp.Data;
 
-// System.IO.File.AppendAllLines("LLLLLLLLOG.txt", ["👉 Starting program! " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")]);
+string logFile = "____log.txt";
+System.IO.File.AppendAllLines(logFile, ["👉 Starting program! " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")]);
 
 try
 {
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
 
-// Console.WriteLine("👉 Connection string: " + builder.Configuration.GetConnectionString("DefaultConnection"));
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    // Console.WriteLine("👉 Connection string: " + builder.Configuration.GetConnectionString("DefaultConnection"));
+    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
-app.UseHttpsRedirection();
-app.UseRouting();
+    app.UseHttpsRedirection();
+    app.UseRouting();
 
-app.UseAuthorization();
+    app.UseAuthorization();
 
-app.MapStaticAssets();
+    app.MapStaticAssets();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
         .WithStaticAssets();
 
     app.Run();
 }
 catch (Exception ex)
 {
-    System.IO.File.AppendAllLines("LLLLLLLLOG.txt", ["👉 Error: " + ex.Message]);
+    System.IO.File.AppendAllLines(logFile, ["👉 Error: " + ex.Message + " " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")]);
     throw;
 }
 
